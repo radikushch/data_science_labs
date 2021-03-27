b38f8f7d5789c135410af02803e36222ae9e001c
Rodion Kushch <rodion.kushch@uptech.team>
2021-03-27 20:19:43 +0200

    cpmplete lab2

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -39,22 +39,65 @@ plt.hist(normalS, bins=20, facecolor="blue", alpha=0.5)
 plt.show()
 #=================================================================
 constantS = np.zeros(n)
-constantS1 = np.zeros(n)
-
-quadraticS = np.zeros(n)
-quadraticS1 = np.zeros(n)
 
 for i in range(n):
     constantS[i] = 0.0000005
+
+mConstantS = np.median(constantS)
+dConstantS = np.var(constantS)
+scvConstantS = mt.sqrt(dConstantS)
+
+print('матриця реалізацій ВВ=',constantS)
+print('матиматичне сподівання ВВ=',mConstantS)
+print('дисперсія ВВ =',dConstantS)
+print('СКВ ВВ=',scvConstantS)
+
+constantS1 = np.zeros(n)
+
+for i in range(n):
     constantS1[i] = constantS[i] + uniformS[i]
 
-    quadraticS[i] = (0.0000005 * i * i)
-    quadraticS1[i] = quadraticS[i] + normalS[i]
+mConstantS1 = np.median(constantS1)
+dConstantS1 = np.var(constantS1)
+scvConstantS1 = mt.sqrt(dConstantS1)
+
+print('матриця реалізацій ВВ=',constantS1)
+print('матиматичне сподівання ВВ=',mConstantS1)
+print('дисперсія ВВ =',dConstantS1)
+print('СКВ ВВ=',scvConstantS1)
 
 plt.plot(constantS1)
 plt.plot(constantS)
 plt.ylabel('динаміка продажів')
 plt.show()
+
+quadraticS = np.zeros(n)
+
+for i in range(n):
+    quadraticS[i] = (0.0000005 * i * i)
+
+mQuadraticS = np.median(quadraticS)
+dQuadraticS = np.var(quadraticS)
+scvQuadraticS = mt.sqrt(dQuadraticS)
+
+print('матриця реалізацій ВВ=',quadraticS)
+print('матиматичне сподівання ВВ=',mQuadraticS)
+print('дисперсія ВВ =',dQuadraticS)
+print('СКВ ВВ=',scvQuadraticS)
+
+quadraticS1 = np.zeros(n)
+
+for i in range(n):
+    quadraticS1[i] = quadraticS[i] + normalS[i]
+
+mQuadraticS1 = np.median(quadraticS1)
+dQuadraticS1 = np.var(quadraticS1)
+scvQuadraticS1 = mt.sqrt(dQuadraticS1)
+
+print('матриця реалізацій ВВ=',quadraticS1)
+print('матиматичне сподівання ВВ=',mQuadraticS1)
+print('дисперсія ВВ =',dQuadraticS1)
+print('СКВ ВВ=',scvQuadraticS1)
 
 plt.plot(quadraticS1)
 plt.plot(quadraticS)
--- lab2/lab2.py
+++ lab2/lab2.py
@@ -2,9 +2,9 @@ import numpy as np
 import math as mt
 import matplotlib.pyplot as plt
 
-n = 100
+n = 10000
 iter = int(n)
-nAVv = 0
+nAVv = 10
 nAV = int((iter * nAVv)/100)
 dm = 0
 dsig = 5
@@ -81,44 +81,27 @@ plt.hist(quadraticSV, bins=20, alpha=0.5, label='S3')
 plt.hist(quadraticSAV, bins=20, alpha=0.5, label='S3')
 plt.show()
 #=================================================================
-e = 0.1
-def half_divide_method(start, end, f, ak):
-    x1 = (start + end) / 2
-    while mt.fabs(f(x1, ak)) >= e:
-        x1 = (start + end) / 2
-        start, end = (start, x1) if f(start, ak) * f(x1, ak) < 0 else (x1, end)
-    return (start + end) / 2
-
 mQuadraticSAV = np.median(quadraticSAV)
 dQuadraticSAV = np.var(quadraticSAV)
 scvQuadraticSAV = mt.sqrt(dQuadraticSAV)
 
 a = 0
 b = 4
-amount = 0
 
-func = lambda x, c: (x + 4) * mt.pow((x - 1), 5) - c * mt.pow((x + 4), 4)
-for i in range(2, n):
-    ai = (quadraticSAV[i] - quadraticSAV[i - 1]) - (quadraticSAV[i - 1] - quadraticSAV[i - 2])
+for i in range(1, n-1):
+    ai = (quadraticSAV[i+1] - quadraticSAV[i]) - (quadraticSAV[i] - quadraticSAV[i-1])
     Ak = i * ai/dQuadraticSAV
     x1 = (a + 4) * mt.pow((a - 1), 5) - Ak * mt.pow((a + 4), 4)
     x2 = (b + 4) * mt.pow((b - 1), 5) - Ak * mt.pow((b + 4), 4)
-    print("x1 = ", x1)
-    print("x2 = ", x2)
     if np.sign(x1) == np.sign(x2):
-        amount = amount + 1
+        if SAV.__contains__(i):
+            quadraticSAV[i] = (quadraticSAV[i-1] + quadraticSAV[i+1])/2
 
-print("Desire AV number = ", nAV)
-print("AV number = ", amount)
+plt.plot(quadraticSAV)
+plt.plot(quadraticS)
+plt.ylabel('динаміка продажів')
+plt.show()
 
-
-# ------------------------------ МНК згладжування -------------------------------------
-Yin = np.zeros(iter)
-F = np.ones((iter, 3))
-for i in range(iter):                          # формування структури вхідних матриць МНК
-    Yin[i] = float(normalS[i])                   # формування матриці вхідних даних без аномілій
-    F[i, 1] = float(i)
-    F[i, 2] = float(i*i)   # формування матриці вхідних даних без аномілій
 # ---------------------- функція обчислень алгоритму - MNK -----------------------------
 def MNK(Yin, F):
     FT = F.T
@@ -128,27 +111,32 @@ def MNK(Yin, F):
     C = FFTIFT.dot(Yin)
     Yout = F.dot(C)
     return Yout
-
+# ------------------------------ МНК згладжування -------------------------------------
+Yin = np.zeros(iter)
+F = np.ones((iter, 3))
+for i in range(iter):                          # формування структури вхідних матриць МНК
+    F[i, 1] = float(i)
+    F[i, 2] = float(i*i)   # формування матриці вхідних даних без аномілій
 # ---------------------- застосування МНК до незашумлених вимірів -----------------------
-Yout0 = MNK (Yin, F)
-print('Yout0=', Yout0, 'Yin=', Yin)
+for i in range(iter):                          # формування структури вхідних матриць МНК
+    Yin[i] = float(quadraticS[i])
+YoutS = MNK(Yin, F)
 # ---------------------- застосування МНК до незашумлених вимірів -----------------------
 for i in range(iter):
     Yin[i] = float(quadraticSV[i])
-Yout1 = MNK(Yin, F)
+YoutSV = MNK(Yin, F)
 # ---------------------- застосування МНК до незашумлених вимірів -----------------------
 for i in range(iter):
     Yin[i] = float(quadraticSAV[i])
-Yout2 = MNK(Yin, F)
+YoutSAV = MNK(Yin, F)
 # ------------ графіки тренда, МНК оцінок нормального та аномального шуму ---------------
-
 Yout00 = np.zeros(n)
 Yout10 = np.zeros(n)
 Yout20 = np.zeros(n)
 for i in range(n):
-     Yout00[i] = abs(Yout0[i] - normalS[i])
-     Yout10[i] = abs(Yout1[i] - normalS[i])
-     Yout20[i] = abs(Yout2[i] - normalS[i])
+     Yout00[i] = abs(YoutS[i] - normalS[i])
+     Yout10[i] = abs(YoutSV[i] - normalS[i])
+     Yout20[i] = abs(YoutSAV[i] - normalS[i])
 
 print('----------------------- статистичны характеристики виміряної вибірки за НАЯВНОСТІ АВ -----------------')
 mYout00=np.median(Yout00);  mYout10=np.median(Yout10);  mYout20=np.median(Yout20)
@@ -162,9 +150,9 @@ print('-------------------------------------------------------------------------
 
 
 plt.plot(Yin)
-plt.plot(Yout0)
-plt.plot(Yout1)
-plt.plot(Yout2)
+plt.plot(YoutS)
+plt.plot(YoutSV)
+plt.plot(YoutSAV)
 plt.ylabel('динаміка продажів')
 plt.show()
 
